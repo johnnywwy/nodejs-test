af7ba73198ff03109bffab752bc8b8805b931eb3
johnnywwy <johnnywwy@gmail.com>
2022-01-02 17:29:49 +0800

    完善了一下，增加了之前的css项目

--- server.js
+++ server.js
@@ -38,13 +38,84 @@ var server = http.createServer(function (request, response) {
         <script src="/y"></script>
         <h1>哈哈哈哈哈，我是二哈</h1>
         <h2>我是新增的内容</h2>
+        <div id="heart">
+            <div class="left"></div>
+            <div class="right"></div>
+            <div class="bottom"></div>
+        </div>
         `)
         response.end()
     }
     else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`* {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+
+
+        #heart {
+            display: inline-block;
+            /* border: 1px solid blue; */
+            margin: 100px;
+            position: relative;
+            /* transition: all 0.5s; */
+
+
+        }
+
+        @keyframes heart {
+            0% {
+                transform: scale(1.0);
+            }
+
+            100% {
+                transform: scale(1.5);
+            }
+        }
+
+        #heart:hover {
+            animation: heart .6s infinite alternate;
+        }
+
+        .left {
+            /* margin: 0 auto; */
+            width: 50px;
+            height: 50px;
+            /* border: 1px solid black; */
+            background: red;
+            transform: rotate(45deg) translateX(35px);
+            position: absolute;
+            bottom: 100%;
+            right: 100%;
+            border-radius: 50% 0 0 50%;
+        }
+
+        .right {
+            /* margin: 0 auto; */
+            width: 50px;
+            height: 50px;
+            /* border: 1px solid blue; */
+            background: red;
+            transform: rotate(45deg) translateY(35px);
+            position: absolute;
+            bottom: 100%;
+            left: 100%;
+            border-radius: 50% 50% 0 0;
+        }
+
+        .bottom {
+            /* margin: 0 auto; */
+            width: 50px;
+            height: 50px;
+            /* border: 1px solid grey; */
+            transform: rotate(45deg);
+            background: red;
+            /* position: absolute; */
+        }`)
         response.end()
     }
     else if (path === '/y') {
